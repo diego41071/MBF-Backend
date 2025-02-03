df9f1d34f1653a16ae10031934a064718a7a4017
diego41071 <alexander.diego.2007@hotmail.com>
2025-02-02 22:45:20 -0500

    cambios

--- src/inventory/inventory.service.ts
+++ src/inventory/inventory.service.ts
@@ -39,33 +39,28 @@ export class InventoryService {
       );
     }
   }
-
   async generatePDF(inventory: any): Promise<Buffer> {
     return new Promise((resolve, reject) => {
       const doc = new PDFDocument();
       const buffers: Buffer[] = [];
 
-      // Capturar los datos generados por el documento en memoria
       doc.on('data', (chunk) => buffers.push(chunk));
       doc.on('end', () => resolve(Buffer.concat(buffers)));
       doc.on('error', (err) => reject(err));
 
       // Encabezado principal
-      doc.rect(50, 30, 515, 75).stroke(); // Rectángulo superior
-      // Agregar imagen al encabezado
-      const cellX = 50; // Posición X de la celda
-      const cellY = 30; // Posición Y de la celda
-      const cellWidth = 130; // Ancho de la celda
+      doc.rect(50, 30, 515, 75).stroke();
 
-      // Dibujar la celda
-      doc.rect(cellX, cellY, cellWidth, 75).stroke();
+      // Logo y empresa
+      const cellX = 50;
+      const cellY = 30;
+      const headerCellWidth = 130;
 
-      // Definir tamaño del logo
-      const logoWidth = 40; // Ancho de la imagen
-      const logoHeight = 40; // Alto de la imagen
+      doc.rect(cellX, cellY, headerCellWidth, 75).stroke();
 
-      // Calcular posición para centrar la imagen en la celda
-      const logoX = cellX + (cellWidth - logoWidth) / 2;
+      const logoWidth = 40;
+      const logoHeight = 40;
+      const logoX = cellX + (headerCellWidth - logoWidth) / 2;
       const logoY = cellY + (50 - logoHeight) / 2;
 
       try {
@@ -77,6 +72,7 @@ export class InventoryService {
       } catch (error) {
         console.error('Error al cargar la imagen:', error.message);
       }
+
       doc.fontSize(8).text('IMPORTACIONES MEDIBÁSCULAS ZOMAC S.A.S.', 40, 40, {
         align: 'center',
         width: 500,
@@ -97,95 +93,79 @@ export class InventoryService {
       // Sección derecha de la cabecera
       doc.rect(400, 30, 165, 75).stroke();
       doc.fontSize(10).text('FICHA TÉCNICA:', 405, 35);
-      doc.fontSize(10).text('FT-145', 500, 35); // Alineado a la derecha de la misma fila
+      doc.fontSize(10).text('FT-145', 500, 35);
+      doc.text('FECHA SERVICIO:', 405, 55);
+      doc.text('23/11/2023', 515, 55);
+      doc.text('PRÓXIMO SERVICIO:', 405, 80);
+      doc.text('23/05/2024', 515, 80);
+      doc.text('PRIORIDAD:', 405, 95);
+      doc.text('ALTA', 530, 95);
 
-      doc.text('FECHA SERVICIO:', 405, 55, {
-        width: 180,
-        align: 'left',
+      // Datos generales en formato 2x2
+      let currentY = 120;
+      const cellHeight = 20;
+      const columnWidth = 150; // Ahora hay solo 2 columnas
+      const startX = 70;
+
+      const generalData = [
+        ['Nombre del Equipo', inventory.name || 'No disponible'],
+        ['Marca', inventory.brand || 'No disponible'],
+        ['Modelo', inventory.model || 'No disponible'],
+        ['Serie', inventory.serialNumber || 'No disponible'],
+        ['Fecha de Compra', inventory.purchaseDate || 'No disponible'],
+        ['Ubicación', inventory.location || 'No disponible'],
+        ['Estado', inventory.status || 'No disponible'],
+        ['Responsable', inventory.responsible || 'No disponible'],
+        ['Proveedor', inventory.supplier || 'No disponible'],
+        ['Garantía', inventory.warranty || 'No disponible'],
+        ['Costo', inventory.cost || 'No disponible'],
+        ['Última Revisión', inventory.lastReview || 'No disponible'],
+        [
+          'Frecuencia de Mantenimiento',
+          inventory.maintenanceFrequency || 'No disponible',
+        ],
+        ['Próxima Inspección', inventory.nextInspection || 'No disponible'],
+        ['Condición Actual', inventory.condition || 'No disponible'],
+        ['Notas Adicionales', inventory.notes || 'No disponible'],
+      ];
+
+      generalData.forEach((row, index) => {
+        const col = index % 2; // Solo 2 columnas ahora
+        const rowNumber = Math.floor(index / 2);
+
+        const x = startX + col * columnWidth;
+        const y = currentY + rowNumber * cellHeight;
+
+        doc.rect(x, y, columnWidth, cellHeight).stroke();
+        doc.text(row[0], x + 5, y + 5);
+        doc.rect(x + columnWidth, y, columnWidth, cellHeight).stroke();
+        doc.text(row[1], x + columnWidth + 5, y + 5);
       });
-      doc.text('23/11/2023', 515, 55, {
-        width: 180,
-        align: 'left',
-      });
-      doc.text('PRÓXIMO SERVICIO:', 405, 80, {
-        width: 180,
-        align: 'left',
-      });
-      doc.text('23/05/2024', 515, 80, {
-        width: 180,
-        align: 'left',
-      });
-      doc.text('PRIORIDAD:', 405, 95, { width: 180, align: 'left' });
 
-      doc.text('ALTA', 530, 95, { width: 180, align: 'left' });
-
-      // Datos generales
-      const startY = 120; // Y inicial para datos generales
-      let currentY = startY;
-
-      const cellHeight = 20; // Altura de cada celda
-      const cellWidthLabel = 130; // Ancho de la celda para la etiqueta
-      const cellWidthValue = 320; // Ancho de la celda para el valor
-
-      // Función para dibujar una fila en celdas
-      const drawRow = (y: number, label: string, value: string) => {
-        // Celda de la etiqueta
-        doc.rect(50, y, cellWidthLabel, cellHeight).stroke();
-        doc.text(label, 55, y + 5, {
-          width: cellWidthLabel - 10,
-          align: 'left',
-        });
-
-        // Celda del valor
-        doc.rect(50 + cellWidthLabel, y, cellWidthValue, cellHeight).stroke();
-        doc.text(value, 55 + cellWidthLabel, y + 5, {
-          width: cellWidthValue - 10,
-          align: 'left',
-        });
-      };
-
-      // Dibujar las filas para los datos generales
-      drawRow(
-        currentY,
-        'Nombre del Equipo:',
-        inventory.name || 'No disponible',
-      );
-      currentY += cellHeight;
-      drawRow(currentY, 'Marca:', inventory.brand || 'No disponible');
-      currentY += cellHeight;
-      drawRow(currentY, 'Modelo:', inventory.model || 'No disponible');
-      currentY += cellHeight;
-      drawRow(currentY, 'Serie:', inventory.serialNumber || 'No disponible');
-      currentY += cellHeight;
-      drawRow(
-        currentY,
-        'Fecha de Compra:',
-        inventory.purchaseDate || 'No disponible',
-      );
+      // Ajustar la posición después de la tabla 2x2
+      currentY += Math.ceil(generalData.length / 2) * cellHeight + 20;
 
       // Especificaciones técnicas
-      currentY += 20; // Espacio adicional antes de la siguiente sección
-      doc.rect(50, currentY, 500, 20).stroke(); // Título de la tabla
+      doc.rect(50, currentY, 500, 20).stroke();
       doc.text('Especificaciones Técnicas', 55, currentY + 5);
       currentY += 25;
 
-      const rows = [
-        ['Capacidad', `${inventory.capacity || 'No disponible'}`],
-        ['Material', `${inventory.material || 'No disponible'}`],
-        ['Voltaje del Equipo', `${inventory.voltage || 'No disponible'}`],
-        ['Peso del Equipo', `${inventory.weight || 'No disponible'}`],
-        ['Potencia del Equipo', `${inventory.power || 'No disponible'}`],
+      const specifications = [
+        ['Capacidad', inventory.capacity || 'No disponible'],
+        ['Material', inventory.material || 'No disponible'],
+        ['Voltaje del Equipo', inventory.voltage || 'No disponible'],
+        ['Peso del Equipo', inventory.weight || 'No disponible'],
+        ['Potencia del Equipo', inventory.power || 'No disponible'],
       ];
 
-      rows.forEach((row) => {
-        doc.rect(50, currentY, 250, 20).stroke(); // Columna 1
+      specifications.forEach((row) => {
+        doc.rect(50, currentY, 250, 20).stroke();
         doc.text(row[0], 55, currentY + 5);
-        doc.rect(300, currentY, 250, 20).stroke(); // Columna 2
+        doc.rect(300, currentY, 250, 20).stroke();
         doc.text(row[1], 305, currentY + 5);
         currentY += 20;
       });
 
-      // Finalizar el documento
       doc.end();
     });
   }
